7d5192cb0c7fd80fc48886eab19c13c930d01f4a
pj0620 <pjm7748@g.rit.edu>
2023-11-06 15:24:38 -0500

    feat: move health check message to properties file

--- src/main/java/com/simplemealplanner/MyApplication.java
+++ src/main/java/com/simplemealplanner/MyApplication.java
@@ -1,6 +1,7 @@
 package com.simplemealplanner;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class MyApplication {
 
+    @Value("${health-check-message: Hello}")
+    private String healthCheckMessage;
+
     @RequestMapping("/")
     String home() {
         log.info("received new health check request");
-        return "Hello World!";
+        return healthCheckMessage;
     }
 
     public static void main(String[] args) {
